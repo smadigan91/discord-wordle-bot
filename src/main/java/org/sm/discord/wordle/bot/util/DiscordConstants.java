a9e7e9f03490ac856b4a22bd2e38dc3a4f30fd9c
Sean <pipgargery@gmail.com>
2022-03-24 20:10:54 -0400

    remove unused constants

--- src/main/java/org/sm/discord/wordle/bot/util/DiscordConstants.java
+++ src/main/java/org/sm/discord/wordle/bot/util/DiscordConstants.java
@@ -5,12 +5,9 @@ package org.sm.discord.wordle.bot.util;
  */
 public class DiscordConstants {
 
-    public static final String BOT_TEST_CHANNEL_ID = "955175071637463051";
     public static final String SPAM_CHANNEL_ID = "311385875076349952";
     public static final String WORDLE_CHANNEL_ID = "930220602147635240";
     public static final String FIRST_SPAM_WORDLE_BLOCK_MESSAGE_ID = "928308959234887751";
     public static final String SECOND_SPAM_WORDLE_BLOCK_MESSAGE_ID = "929827520629583892";
-    public static final String FIRST_SPAM_WORDLE_MESSAGE_ID = "928309675047391264";
-    public static final String LAST_SPAM_WORDLE_MESSAGE_ID = "930156958579847229";
 
 }
